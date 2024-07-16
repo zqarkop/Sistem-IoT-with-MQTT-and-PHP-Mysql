edd0d010791ee9654b3378a8d44743d2e94914ce
Andi Saputra <86837025+zqarkop@users.noreply.github.com>
2024-07-16 20:46:01 +0700

    Update header.php

--- Website Code/sistemiot/inc/header.php
+++ Website Code/sistemiot/inc/header.php
@@ -9,7 +9,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Sistem IoT by Andi Saputra</title>
-  <script src="https://unpkg.com/mqtt/dist/mqtt.min.js"></script>
+  <script src="../inc/mqtt.min.js"></script>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
